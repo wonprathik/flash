03aaf67204f42c343b1628201a0dbc77df7e9760
wonderla <wonderla@VINOD>
2019-11-12 17:34:04 +0530

    First commit

---
+++ myproject/src/myproject/MyClass.java
@@ -0,0 +1,13 @@
+package myproject;
+
+public class MyClass {
+
+	public static void main(String args[])
+	{
+		
+		System.out.println("Hello world yes");
+		
+		
+	}
+	 
+}
---
+++ myproject/src/myproject/Run.java
@@ -0,0 +1,26 @@
+package myproject;
+
+class Run
+
+{
+	
+	public static void main(String args[])
+	
+	{
+		Student s = new Student();
+		
+		 s.setId(15);
+		 
+		 System.out.println(s.getId());
+		 
+		 s.setName("Manju");
+		 
+		 System.out.println(s.getName());
+		 
+		 s.setAge(37);
+		
+		 System.out.println(s.getAge());
+		
+		
+	}
+}
